6d52932b9a19a34cd2f78604521706fe980416e6
Anders Hansen-Haug <anha1607@student.miun.se>
2018-05-09 10:50:57 +0200

    Start

---
+++ SuFitApp/SuFitApp/SuFitApp.Android/DBClasses/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SuFitApp.Droid.DBClasses
+{
+    public class Person
+    {
+        public int id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Gender { get; set; }
+        public int Age { get; set; }
+        public int Length { get; set; }
+        public int Weight { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public List<Person> AllPersons { get; set; }
+
+        public Person()
+        {
+            AllPersons = new List<Person>();
+        }
+
+        public Person(string firstname, string lastname)
+        {
+            firstname = Firstname;
+            lastname = Lastname;
+        }
+    }
+}
---
+++ SuFitApp/SuFitApp/SuFitApp.Android/DBOperation/DBConn.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SuFitApp.Droid.DBClasses;
+using Npgsql;
+
+namespace SuFitApp.Droid.DBOperation
+{
+    public class DBconn
+    {
+        private NpgsqlConnection conn = new NpgsqlConnection("Server=horton.elephantsql.com; Port=5432; User Id=amnszqbm; Password=-G_Di_7kaJdyfHf4-KL9BPvDmLZ6r55d; Database=amnszqbm");
+        private NpgsqlCommand cmd;
+        private NpgsqlDataReader dr;
+
+
+
+        public List<Person> DBCall(List<Person> list)
+        {
+            cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM person";
+            try
+            {
+                conn.Open();
+                dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    list.Add(new Person(dr["firstname"].ToString(), dr["lastname"].ToString()));
+                }
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+    }
+}
